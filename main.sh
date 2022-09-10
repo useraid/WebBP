c3baa667d485de46768da2a1833c209309db078e
useraid <useraid@pm.me>
2022-09-10 16:14:25 +0530

    added LAMP stack and updated docker check

--- main.sh
+++ main.sh
@@ -25,26 +25,89 @@ function help {
 cat <<EOF
 
 
-This program generates a Boilerplate template for you web applications.
+This program generates a Boilerplate template for your web applications.
     WARNING: Some of the functionality in this program requires root privileges. Use at
 your own risk.
 
     options:
 
-        -h|--help                Display all options and flags. 
+        -h                Display all options and flags. 
+
+        -l                LAMP Stack
+
+        -m                MERN Stack
 
 
 EOF
 }
 
+# Variables
+
+TIMEZONE=$(cat /etc/timezone)
+SQLRPASS="rootpwd"
+SQLUSER="user"
+SQLPASS="password"
+SQLDB="newdb"
+
+
 # Docker Installation Check
 
 function docker {
+  # Curl
+  if ! command -v curl &> /dev/null
+  then
+      sudo apt-get -y install curl
+  fi
+  # Docker
   if ! command -v docker &> /dev/null
   then
-      curl -fsSL https://get.docker.com -o docker.sh
-      sh docker.sh
+      curl -sSL https://get.docker.com/ | sh
+      sudo usermod -aG docker $USER
+      newgrp docker
+      sudo chmod a+x /usr/local/bin/docker-compose
   fi
+}
+
+# LAMP Stack
+
+function lamp {
+  mkdir -p ~/lamp/webroot
+  touch ~/lamp/docker-compose.yml
+  cat << EOF >> ~/lamp/docker-compose.yml
+version: '3.7'
+
+services:
+    php-httpd:
+        image: php:apache
+        ports:
+            - 80:80
+        volumes:
+            - "./webroot:/var/www/html"
+
+    mariadb:
+        image: mariadb:latest
+        volumes:
+            - mariadb-volume:/var/lib/mysql
+        environment:
+            TZ: "$TIMEZONE"
+            MYSQL_ALLOW_EMPTY_PASSWORD: "no"
+            MYSQL_ROOT_PASSWORD: "$SQLRPASS"
+            MYSQL_USER: "$SQLUSER"
+            MYSQL_PASSWORD: "$SQLPASS"
+            MYSQL_DATABASE: "$SQLDB
+
+    phpmyadmin:
+        image: phpmyadmin/phpmyadmin
+        links:
+            - 'mariadb:db'
+        ports:
+            - 8081:80
+
+volumes:
+    mariadb-volume:
+
+EOF
+sudo docker-compose up -f ~/lamp/docker-compose.yml -d --build
 }
 
 # Selection Flags
